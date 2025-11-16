53e2d2c5f17c429c297a2530563bce2997da9066
Lysenko Dmitry <dimondeya@icloud.com>
2025-11-16 13:59:09 +0200

    Task Complete

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const userName = 'Dmitry';
+const userAge = 20;
+const isReady = true;
+console.log(`User: ${userName}, Age: ${userAge}, Boolean: ${isReady}`);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const userName: string = 'Dmitry';
+const userAge: number = 20;
+const isReady: boolean = true;
+
+console.log(`User: ${userName}, Age: ${userAge}, Boolean: ${isReady}`);
+
